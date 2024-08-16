0ebb03767779561dede3280f6a97b7060cb03fa0
ChloeCrozier <chloecrozier@gmail.com>
2024-08-16 16:09:45 -0400

    initial commit

---
+++ pomodoro_timer/pomodoro_timer Watch App/pomodoro_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoro_timerApp.swift
+//  pomodoro_timer Watch App
+//
+//  Created by Chloe Crozier on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct pomodoro_timer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pomodoro_timer/pomodoro_timer Watch AppUITests/pomodoro_timer_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  pomodoro_timer_Watch_AppUITestsLaunchTests.swift
+//  pomodoro_timer Watch AppUITests
+//
+//  Created by Chloe Crozier on 8/16/24.
+//
+
+import XCTest
+
+final class pomodoro_timer_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
